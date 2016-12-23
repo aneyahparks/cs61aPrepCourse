93ff44c356ad0361192dd54719215d8a3c1d2da0
Aneyah Parks <Aneyah Parks>
2016-12-23 15:34:22 -0800

    Add week one homework

---
+++ Chapter5/FullNameGreeting.rb
@@ -0,0 +1,14 @@
+puts 'Hello there! What\'s your name?'
+puts 'Wait! No, I wanna guess!'
+puts 'What is your first name?'
+firstname = gets.chomp
+puts 'Ok, ' + firstname + '...'
+puts 'What\'s your middle name?'
+middlename = gets.chomp
+puts 'So, ' + firstname + ' ' + middlename + '...'
+puts 'And you\'re last name?'
+lastname = gets.chomp
+puts 'YAY!!! ' + firstname + ' ' + middlename + ' ' + lastname
+puts 'What a lovely name. :)'
+puts 'I\'m a great guesser... aren\'t I?'
+puts gets
